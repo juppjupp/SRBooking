353ff9a623d239b3f753497b2a7599fa033d6deb
Jonas Ericsson <jonasericsson1@hotmail.se>
2014-03-25 16:46:30 +0100

    + Added project

---
+++ SRBooking/SRBookingTests/SRBookingTests.m
@@ -0,0 +1,34 @@
+//
+//  SRBookingTests.m
+//  SRBookingTests
+//
+//  Created by Jonas Ericsson on 25/03/14.
+//  Copyright (c) 2014 joersolution.se. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SRBookingTests : XCTestCase
+
+@end
+
+@implementation SRBookingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
