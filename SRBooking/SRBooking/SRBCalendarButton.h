5017e4668a296eaf690f3e4f75ae26c5f1389283
Jonas Ericsson <jonasericsson1@hotmail.se>
2014-03-29 09:24:26 +0100

    + Added calendarButton

---
+++ SRBooking/SRBooking/SRBCalendarButton.h
@@ -0,0 +1,13 @@
+//
+//  SRBCalendarButton.h
+//  SRBooking
+//
+//  Created by Jonas Ericsson on 28/03/14.
+//  Copyright (c) 2014 joersolution.se. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRBCalendarButton : UIButton
+
+@end
